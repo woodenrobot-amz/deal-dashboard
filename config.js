984c70ad6c0a9f16f2e7e6edefefe141b3a491d0
woodenrobot-amz <andy.adams85@gmail.com>
2026-05-12 13:53:38 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ const CONFIG = {
     WEIGHT_SAVINGS: 1.5,
     WEIGHT_RATING: 5,
     MIN_RATING: 4.0,
-    // THE NEW SOURCE LIBRARY
+
+    // THE SOURCE LIBRARY
     SOURCES: {
         woodworking: {
             name: "Wooden Robot Deals",
@@ -17,8 +18,8 @@ const CONFIG = {
             name: "Deals for Dudes",
             url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJRzV2dgrkg8XOyW5_RD4gjiX04ZBFaYk2kalQJ96Gm0ILosp6tgwdvbNGGW3Un36Qomsl3G07XJc5/pub?output=csv"
         }
-    BRAND_LIST_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQPpOLeIO08oXPJw7ZzdewvxjRGDSuHN0ZzWQojlCDSobCIBNWwXH8kMbC97aUW11HJZFe0POGHrDWw/pub?output=csv",
-    // ...
-};
-    }
+    }, // This comma and brace close the SOURCES object
+
+    // THE MASTER BRAND LIST
+    BRAND_LIST_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQPpOLeIO08oXPJw7ZzdewvxjRGDSuHN0ZzWQojlCDSobCIBNWwXH8kMbC97aUW11HJZFe0POGHrDWw/pub?output=csv"
 };
